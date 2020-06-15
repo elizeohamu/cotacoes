30c344ac8cbbcd2732c9e2bf6140c1019b4ace68
Elízeo <elizeohamu@ufg.br>
2020-06-15 17:02:59 -0300

    tracando porta

--- public/js/app.js
+++ public/js/app.js
@@ -21,7 +21,7 @@ cotacoesForm.addEventListener('submit', (event) => {
         mainMensage.innerText = 'O ativo deve ser informado'         
         return
     }
-    fetch(`http://localhost:3000/cotacoes?ativo=${ativo}`).then((response) => {
+    fetch(`/cotacoes?ativo=${ativo}`).then((response) => {
         response.json().then((data) => {         
             if(data.error){
                 mainMensage.innerText = 'Alguma coisa deu errado' 
--- src/app.js
+++ src/app.js
@@ -80,6 +80,8 @@ app.get('*', (req, res) => {
     })
 })
 
+const port = process.env.PORT || 3000
+
 app.listen(3000, () => {
-    console.log('Server is on port 3000')
+    console.log(`Server is on port ${port}`)
 })
